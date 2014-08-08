a036158b615d370189d1b65d802db4effb798a32
Ashish Datta <ashish@setfive.com>
2014-08-08 00:30:19 -0400

    - sorted out how to sha2

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,7 @@ import play.api.mvc.{Action, Controller}
 import anorm._
 import play.api.db.DB
 import play.api.Play.current
+import java.security.MessageDigest
 import models._
 
 object Application extends Controller {
@@ -17,6 +18,9 @@ object Application extends Controller {
     
     val targetPhrase = ("Maybe she's born with it. Maybe it's %.").split("%").toList
     val availableWords = Array("Horse meat", "Just the tip", "One thousand Slim Jims", "Sexual humiliation").toList
+       
+    val str = "This is a test"
+    println( play.api.libs.Codecs.toHexString(MessageDigest.getInstance("SHA-256").digest(str.getBytes())) )
     
     Ok( views.html.game(game.get, targetPhrase, availableWords) )
   }
