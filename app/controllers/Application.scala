621987a49ce4afb97b0d2689fa83df687e549600
Ashish Datta <ashish@setfive.com>
2014-08-09 18:27:05 -0400

    - added more golf code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,36 +1,53 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import models._
 import anorm._
+import play.api.mvc.{Action, Controller}
 import play.api.db.DB
 import play.api.Play.current
-import models._
+import play.api.libs.json._
 
 object Application extends Controller {
+  
+  def getHash = Action(parse.json) {request => {
     
+    val jsonResult = request.body match {
+	    case JsObject(fields) => {
+	      val phrase = fields.find(a => a._1 == "phrase").get._2.toString
+	      val hash = GamePhrase.calculatePhraseHash(phrase)
+	      val id = Integer.valueOf(fields.find(a => a._1 == "id").get._2.toString)	      
+	      
+	      Json.toJson( Json.obj("error" -> false, "calculatedHash" -> hash, 
+	    		  				"isMatch" -> GamePhrase.doesHashMatch(hash, AvailableKeys.find(id).get)) )
+	    }
+	    case _ => Json.toJson( Json.obj("error" -> true, "msg" -> "Sorry! That isn't valid JSON.") )
+	  }
+    
+    Ok( jsonResult )
+  }} 
+  
   def game(id: Int) = Action {
-    val game = AvailableKeys.find(id)
     
+    val game = AvailableKeys.find(id)    
     if( game.isEmpty ){
       sys.error("Sorry! That key doesn't exist")
     }
     
+    val availableWords = GamePhrase.getRandomPhrases
     val gamePhrase = GamePhrase.getRandomBlankPhrase
+    
     val targetPhrase = {
       if( gamePhrase.phrase.split("%").length == 1 ){         
         gamePhrase.phrase.split("%").toList :+ "."
       }else{
         gamePhrase.phrase.split("%").toList  
 	  }
-    }
-    
-    val availableWords = Array("Horse meat", "Just the tip", "One thousand Slim Jims", "Sexual humiliation").toList
+    }    
     
     Ok( views.html.game(game.get, targetPhrase, availableWords) )
   }
   
-  def index = Action {        
-        
+  def index = Action {                
     val availableKeys = AvailableKeys.getAllAvailableKeys
     
     Ok( views.html.index(availableKeys) )
--- app/models/GamePhrase.scala
+++ app/models/GamePhrase.scala
@@ -5,6 +5,8 @@ import anorm.SqlParser._
 import play.api.db.DB
 import play.api.Play.current
 import scala.collection._
+import play.api.libs.Codecs
+import java.security.MessageDigest
 
 abstract class BaseGamePhrase(id: Integer, pharse: String) {  
   def id: Integer
@@ -16,26 +18,48 @@ case class Phrase(id: Integer, phrase: String) extends BaseGamePhrase(id, phrase
 
 object GamePhrase {
 
-  val TYPE_WITH_BLANK_PHRASE = "1"
-  val TYPE_PHRASE = "2"
+  val TYPE_WITH_BLANK_PHRASE = 1
+  val TYPE_PHRASE = 2
   
-  def rowParser = {
+  def calculatePhraseHash(phrase: String): String = {
+    val sha = MessageDigest.getInstance("SHA-256")    
+    Codecs.toHexString(sha.digest(Codecs.toHexString(sha.digest(phrase.getBytes())).getBytes()))    
+  }
+  
+  def doesHashMatch(hash: String, key: AvailableKey): Boolean = {
+    key.publicKey.last == hash.last
+  }
+  
+  def rowParser() = {
     
-    (int("id") ~ str("phrase")).map(f => {      
-      f match {    
-	  	case i~p => PhraseWithBlank(i, p)
+    (int("id") ~ str("phrase") ~ int("phrase_type")).map(f => {      
+      f match {
+	  	case i~p~t => 
+	  	  if( t == TYPE_WITH_BLANK_PHRASE ){ 
+	  	    PhraseWithBlank(i, p)
+	  	  }else{
+	  	    Phrase(i, p)
+	  	  }
       }
     })
     
   }
     
-  def getRandomBlankPhrase: PhraseWithBlank = {
+  def getRandomPhrases: List[BaseGamePhrase] = {
+    DB.withConnection(implicit c => {
+      SQL("SELECT * FROM golf_phrase WHERE phrase_type = {type} ORDER BY RAND() LIMIT 4")
+      	 .on("type" -> TYPE_PHRASE)
+      	 .as(rowParser *)
+    })    
+  }
+  
+  def getRandomBlankPhrase: BaseGamePhrase = {
           
     DB.withConnection(implicit c => {
       SQL("SELECT * FROM golf_phrase WHERE phrase_type = {type} ORDER BY RAND() LIMIT 1")
       	 .on("type" -> TYPE_WITH_BLANK_PHRASE)
       	 .as(rowParser.single)
-    })    
+    })
     
   }
   
